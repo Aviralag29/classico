3f0fe944f5c2d74a7b6fad2cd897bad1aec783fc
Aviral <avirala368@gmail.com>
2023-05-19 14:23:49 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
+import 'package:classico/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    
+    // double pi= 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = " Tuesday ";
+    // const pi = 3.14;  // once define it can not be changed 
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-        child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
